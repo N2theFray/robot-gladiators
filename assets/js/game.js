77760451cb7a933256b924a234789756bb993bdb
Scuba Steve <78673754+blacksteven1@users.noreply.github.com>
2021-02-26 08:14:47 +0300

    played with js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
 window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt ("What is your robot's name?");
+//window.alert(playerName);
+//the following is so we don't get 1000 prompts
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself messages");
+//this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+// another example of how to use console log with var
+console.log("Your robot, " + playerName ", has won!!");
+// this creatres a function named "fight"
+function fight () {
+    window.alert ("The fight has begun!");
+}
+//fight ();
